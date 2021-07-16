bb08f64a2aac6e03fa1d3bffc5d78b362f29d0da
Nchampion-eni <75608487+Nchampion-eni@users.noreply.github.com>
2021-07-16 17:04:17 +0200

    v1.2

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,6 @@ return [
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Sonata\CacheBundle\SonataCacheBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
